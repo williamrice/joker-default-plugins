c505b0c3cd38b23c82d03863f616cd524af9c9e7
William Rice <billyrice12@live.com>
2024-04-17 09:18:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,27 @@ cd plugins
 # Recursively unzip each zip folder inside to a folder of the same name
 find . -type f -name "*.zip" -exec wp plugin install {} \;
 
+#remove default plugins
+wp plugin uninstall akismet
+wp plugin uninstall hello
+
+#activate plugins
+wp plugin activate --all
+
 # Navigate to the themes folder
 cd ../themes
 
 # Recursively unzip each zip folder inside
 find . -type f -name "*.zip" -exec wp theme install {} \;
+
+#uninstall default themes leaving 1 fallback thentytwentyfour
+wp theme uninstall twentytwentytwo
+wp theme uninstall twentytwentythree
+
+#cleanup
+cd ..
+rm -rf repo
+
+
+
 
